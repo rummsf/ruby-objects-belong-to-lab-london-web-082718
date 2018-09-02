99fd52c0cf701a646563f03d7b89ce82aaa8685c
Irum F <irummf@gmail.com>
2018-09-02 17:34:36 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist 
+  
+  attr_accessor :name
+  
+  def initialize
+    @name = name
+  end
+  
+  def name
+    @name
+  end 
+end 
---
+++ lib/author.rb
@@ -0,0 +1,12 @@
+class Author
+  
+  attr_accessor :name  
+  
+  def initialize
+    @name = name 
+  end 
+  
+  def name 
+    @name  
+  end 
+end 
